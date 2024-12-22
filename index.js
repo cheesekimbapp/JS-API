c12fd40e13f1d21b87eb95b1c6e1f2b838fbd8db
Muhammad Muchtar Syarifrudin <syarifrudinmuchtar@students.unnes.ac.id>
2024-12-22 16:56:21 +0700

    Second commit to add post and put

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 // Routes/URL/endpoint utama dengan method GET
 app.get('/', (request, response) => {
-    response.send('Hello')
+    response.send('Hello lalalala')
 })
 
+// Routes/URL/endpoint utama dengan method GET untuk menampilkan data
 app.get('/hello', (req, res) => {
-  res.send('Hello World!')
+    console.log({ urlParam: req.query})
+    res.send('Hello World!')
 })
 
+// Routes/URL/endpoint utama dengan method POST
 app.post('/login', (req, res) => {
-    if(req.name === "muchtar") {
+    console.log({ requestFromOutside: req.body })
         res.send('Login Success')
-    }
 })
+// Routes/ULR/endpoint utama dengan method PUT
+app.put('/username', (req, res) => {
+    console.log({ updateData: req.body })
+        res.send('Update Success')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
