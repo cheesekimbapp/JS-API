31d202475fadb0846affc5b23bfa25b0a5e415b9
Muhammad Muchtar Syarifrudin <syarifrudinmuchtar@students.unnes.ac.id>
2024-12-22 15:15:18 +0700

    Testing routes using get method

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const data = "testing"
-console.log(data)
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Routes/URL/endpoint utama dengan method GET
+app.get('/', (request, response) => {
+    response.send('Hello')
+})
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/login', (req, res) => {
+    if(req.name === "muchtar") {
+        res.send('Login Success')
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
